3451da3d7bb6b1f20617a4ecefd0befb708a3df0
Dmitriy <unzippo@gmail.com>
2019-08-05 02:33:45 +0300

    1,2,3

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -1,4 +1,21 @@
 package ru.skillbranch.devintensive
 
-class App {
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+    companion object {
+        private var instance: App? = null
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
 }
